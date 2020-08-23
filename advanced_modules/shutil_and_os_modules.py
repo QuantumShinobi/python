a9e7728c20a8cae068341fca23f040a251068829
IamEinstein1 <vermarishit039@gmail.com>
2020-08-23 12:32:01 +0530

    done for the day

--- advanced_modules/shutil_and_os_modules.py
+++ advanced_modules/shutil_and_os_modules.py
@@ -1,4 +1,5 @@
 # %%
+import send2trash
 import os
 import shutil
 f = open("practice.txt", "w+")
@@ -11,4 +12,43 @@ os.getcwd()
 # %%
 # to list items in the current directory
 os.listdir()
+os.listdir(path="c:\\Users")
 # %%
+# to move files
+# takes src and destination to move the files
+shutil.move('practice.txt', "C:\\Users\\Shalini\\Desktop")
+# %%
+os.listdir(path="C:\\Users\\Shalini\\Desktop")
+
+# %%
+#  to delete files
+#  there are 3 ways
+#  1. os.unlink(path) => this deletes a file in a path
+#  2. os.rmdir(path) => removes a folder
+#  3. shutil.rmtree(path) => removes ALL THE FILES AND FOLDERS IN A PATH
+# all of these methods are irreversible
+# the files will be permanentaly deleted
+
+
+# so to make it reverse
+# a module called  "send2trash" is used
+# with send2trash
+# send2trash.send2trash
+# %%
+# %%
+os.listdir()
+# %%
+shutil.move("C:\\Users\\Shalini\\Desktop\\practice.txt", os.getcwd())
+
+# %%
+send2trash.send2trash('practice.txt')
+# %%
+os.listdir()
+
+
+# %%
+# it will yield a tuple for all items in a diectory
+# os.walk()
+# %%
+# for folder, sub_folders, files in os.walk()
+# do this yourself
