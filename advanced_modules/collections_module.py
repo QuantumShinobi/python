2fb22e597672ad250ea5228de9104f0b77d7a077
IamEinstein1 <vermarishit039@gmail.com>
2020-08-23 10:32:42 +0530

    git commit

--- advanced_modules/collections_module.py
+++ advanced_modules/collections_module.py
@@ -1,4 +1,5 @@
 # %%
+from collections import namedtuple
 from collections import defaultdict
 from collections import Counter
 
@@ -32,4 +33,54 @@ d
 # %%
 
 # %%
+d = {'a': 10}
+# %%
+d
+# %%
+d['a']
+
+
+# %%
+d[10]
+# %%
+d['wrong']
+# %%
+#   a default dictionary will assign a default value to a key
+#  even if it was not created
+#  hence no error will be recieved
+# %%
+d = defaultdict(lambda: 0)
+
+# %%
+d['ok'] = 100
+# %%
+d['ok']
+
+# %%
+d['no key']
+# %%
+
+# %%
+d
+# %%
+# %%
+mytuple = (10, 20, 30)
+# %%
+mytuple[0]
+# %%
+Dog = namedtuple('DOG', ['age', 'breed', 'name'])
+# %%
+sam = Dog(age=5, breed="husk", name="sam")
+# %%
+sam
+# %%
+sam.age
+# %%
+sam.name
+# %%
+sam.name
+# %%
+sam[1]
+# %%
+sam[0]
 # %%
---
+++ advanced_modules/shutil_and_os_modules.py
@@ -0,0 +1,14 @@
+# %%
+import os
+import shutil
+f = open("practice.txt", "w+")
+f.write("this is a text screen ")
+f.close()
+
+# %%
+# current directory
+os.getcwd()
+# %%
+# to list items in the current directory
+os.listdir()
+# %%
