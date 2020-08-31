81361635f4c53bfedcebf9506ea32a9ece49c7df
IamEinstein1 <vermarishit039@gmail.com>
2020-08-31 10:18:36 +0530

    done with classes

--- webscraping/itemsbyclass.py
+++ webscraping/itemsbyclass.py
@@ -1,2 +1,24 @@
+
+# soup.select('div')
+# soup.select("#id")
+# soup.select(".class")
+# soup.select("div span")
+# 👆 this gives :-  all the spans in a div
+# soup.select("div>span")
+# 👆 this gives :-  all the spans direclty in a div
+
+# %%
 import bs4
 import requests
+
+# %%
+res = requests.get("https://en.wikipedia.org/wiki/Visual_Studio_Code")
+soup = bs4.BeautifulSoup(res.text, 'lxml')
+
+# %%
+result = soup.select('.toctext')
+# %%
+for item in result:
+    print(item.text)
+
+# %%
